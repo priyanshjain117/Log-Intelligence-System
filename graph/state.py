dc805c7bd32014d3ca2c9e43f9093ceb47bf928b
priyanshjain117 <jainpriyanshu117@gmail.com>
2026-04-12 20:42:42 +0530

    fixing state

--- graph/state.py
+++ graph/state.py
@@ -1,8 +1,9 @@
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, Any
 
 class LogState(TypedDict):
     log: str
-    log_vector: Optional[list[float]]  # for vector DB search
+    clean_text: Optional[str]          # Added
+    log_vector: Optional[list[float]]  
 
     # Regex
     regex_label: Optional[str]
@@ -14,10 +15,16 @@ class LogState(TypedDict):
     # Vector DB
     vector_label: Optional[str]
     vector_score: Optional[float]
+    vector_context: Optional[list]     # Added
 
     # LLM
-    llm_response: Optional[str]
+    llm_response: Optional[dict]       # Changed to dict (or Any)
+
+    validation_passed: Optional[bool]
+    validation_feedback: Optional[str]
 
     # Control
     final_label: Optional[str]
+    final_source: Optional[str]        # Added
+    final_reason: Optional[str]        # Added
     attempts: int
