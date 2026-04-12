51e68e02d86478414cf0287fcfb08038452699b0
priyanshjain117 <jainpriyanshu117@gmail.com>
2026-04-12 14:00:00 +0530

    embedding loder updated in code

--- utils/loader.py
+++ utils/loader.py
@@ -2,13 +2,19 @@ import joblib
 from dotenv import load_dotenv
 import os
 
+from sentence_transformers import SentenceTransformer
 load_dotenv()
 
 model_path = os.getenv("ML_MODEL_PATH", "models/ml_model.joblib")
 label_encoder_path = os.getenv("LABEL_ENCODER_PATH", "models/label_encoder.joblib")
+embedding_model = "all-MiniLM-L6-v2"
+
 
 def load_ml_model():
     return joblib.load(model_path)
 
 def load_label_encoder():
     return joblib.load(label_encoder_path)
+ 
+def load_embedding_model():
+    return SentenceTransformer(embedding_model)
