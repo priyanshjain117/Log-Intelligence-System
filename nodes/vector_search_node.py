c0a375bec540eb669df4ed0ed9a9512178e0f60d
priyanshjain117 <jainpriyanshu117@gmail.com>
2026-04-12 14:02:49 +0530

    vector chahce setuped

---
+++ nodes/vector_search_node.py
@@ -0,0 +1,30 @@
+from utils.vector_db import VectorDB
+from .ml_node import embedding_model
+
+dim = len(embedding_model.encode(["test"])[0])
+
+vector_db = VectorDB(dim)
+
+def vector_search_node(state):
+    vector = state.get("log_vector")
+
+    if vector is None:
+        return {
+            "vector_label": None,
+            "vector_score": 0.0
+        }
+
+    results = vector_db.search(vector)
+
+    if not results:
+        return {
+            "vector_label": None,
+            "vector_score": 0.0
+        }
+
+    best = results[0]
+
+    return {
+        "vector_label": best["label"],
+        "vector_score": best["score"]
+    }
